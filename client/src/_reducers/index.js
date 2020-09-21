fe54699a36e7402685b055103ffe6951083a15c8
HanJeong Seol <hanjeongseol@HanJeongui-MacBookPro.local>
2020-09-21 16:33:27 +0900

    redux & middleware add & basic file create

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,15 @@
+import { combineReduxers } from "redux"
+// import user from "./user_reducer"
+// import comment from "./comment_reducer"
+
+// combineReduxers : Store에 여러가지 reducer가 존재할 수 있다.
+// state가 변화한 후에 변화한 값을 리턴하는게 reducer.
+// state가 user, post 등등 여러가지 존재할 수 있기 때문에
+// combineReduxers를 사용해서 rootReducer에서 하나로 합쳐준다.
+// 우리는 login register를 만들기 때문에 user reducer를 생성한다.
+const rootReducer = combineReduxers({
+    // user,
+    // comment
+})
+
+export default rootReducer
--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,26 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
 import * as serviceWorker from "./serviceWorker"
-
+import { Provider } from "react-redux"
 import "antd/dist/antd.css"
+import { applyMiddleware } from "redux"
+import promiseMiddleware from "redux-promise"
+import ReduxThunk from "redux-thunk"
+import Reducer from "./_reducers"
+
+// redux는 객체만 받기 때문에 promise와 func 받도록 한다.
+const createStorewithMiddleare = applyMiddleware(promiseMiddleware, ReduxThunk(createStore))
 
 ReactDOM.render(
-    <React.StrictMode>
+    <Provider
+        store={createStorewithMiddleare(
+            Reducer,
+            // chrome Redux Extension connect
+            window.__REDUX_DEVTOOLS_EXTESION__ && window.__REDUX_DEVTOOLS_EXTESION__()
+        )}
+    >
         <App />
-    </React.StrictMode>,
+    </Provider>,
     document.getElementById("root")
 )
 
